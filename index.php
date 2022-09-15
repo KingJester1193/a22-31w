583aaaa6e993fd6802e5d89e66faefe8c1e5ba75
KingJester1193 <102245723+KingJester1193@users.noreply.github.com>
2022-09-15 11:15:19 -0400

    ajout balise style

--- index.php
+++ index.php
@@ -19,6 +19,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <style>body{
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+}
+
+h1{
+    background-color: rgb(35, 156, 116);
+    color: rgb(255, 166, 0);
+    font-size: 2em;
+}</style>
+
+
+
     <title>Document</title>
  </head>
  <body>
