ba016e1e7b13bb5fdd06742871664ddc172b2ab5
KingJester1193 <102245723+KingJester1193@users.noreply.github.com>
2022-09-15 10:07:00 -0400

    modified the_title()

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
         /* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-                the_title( );
-                the_content();
+                the_title( "<h1>", "</h1>");
+                the_content(null, true);
             endwhile;
         endif;
     ?>
