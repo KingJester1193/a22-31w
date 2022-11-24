9e9548a7c301fe8d33167ed117c3ed47af2ccc6d
KingJester1193 <102245723+KingJester1193@users.noreply.github.com>
2022-11-24 11:08:49 -0500

    ef-personalisation de la page recherche

---
+++ template-parts/content-search.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template part for displaying results in search pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+		<?php
+			global $wp_query;
+			$total_results = $wp_query->found_posts;
+			?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+
+
+	<div class="entry-summary">
+		<?php echo wp_trim_words( get_the_excerpt(), 20,);?><a href="<?php the_permalink();?> "><svg> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="30.729px" height="30.729px" viewBox="0 0 30.729 30.729" style="enable-background:new 0 0 30.729 30.729;" xml:space="preserve">
+<g>
+	<path d="M24.813,15.366L10.185,29.997c-0.487,0.487-1.128,0.731-1.768,0.731c-0.641,0-1.279-0.244-1.769-0.731   c-0.977-0.978-0.977-2.561,0-3.536l11.095-11.096L6.649,4.268c-0.976-0.977-0.976-2.56,0-3.536c0.977-0.977,2.56-0.976,3.536,0   L24.813,15.366z"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg></svg></a>
+		
+	</div><!-- .entry-summary -->
+
+	<footer class="entry-footer">
+	
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
